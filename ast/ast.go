9e8a251d1e166fadc6d78f2cab6b7015aa192afe
jmarchesini <john.marchesini@gmail.com>
2022-08-10 13:42:51 -0500

    Test refactor and add boolean support

--- ast/ast.go
+++ ast/ast.go
@@ -215,3 +215,21 @@ func (il *IntegerLiteral) TokenLiteral() string {
 func (il *IntegerLiteral) String() string {
 	return il.Token.Literal
 }
+
+// //////////////////////////////////////////////// //
+// =============== BooleanLiteral ================= //
+// //////////////////////////////////////////////// //
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
