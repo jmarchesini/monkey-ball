afbf20cd76d5f2d16dc0ca4c352739ff7fce8419
jmarchesini <john.marchesini@gmail.com>
2022-08-16 12:21:47 -0500

    3.9: env

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -12,64 +12,76 @@ var (
 	FALSE = &object.Boolean{Value: false}
 )
 
-func Eval(node ast.Node) object.Object {
+func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 
 	// ==== Statements ==== //
 	case *ast.Program:
-		return evalProgram(node.Statements)
-	case *ast.ExpressionStatement:
-		return Eval(node.Expression)
-	case *ast.BlockStatement:
-		return evalBlockStatement(node)
-	case *ast.ReturnStatement:
-		val := Eval(node.ReturnValue)
+		return evalProgram(node.Statements, env)
 
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression, env)
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node, env)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue, env)
 		if isError(val) {
 			return val
 		}
-
 		return &object.ReturnValue{Value: val}
+
+	case *ast.LetStatement:
+		val := Eval(node.Value, env)
+		if isError(val) {
+			return val
+		}
+		env.Set(node.Name.Value, val)
 
 	// ==== Expressions ==== //
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
-	case *ast.PrefixExpression:
-		right := Eval(node.Right)
 
+	case *ast.PrefixExpression:
+		right := Eval(node.Right, env)
 		if isError(right) {
 			return right
 		}
-
 		return evalPrefixExpression(node.Operator, right)
-	case *ast.InfixExpression:
-		left := Eval(node.Left)
 
+	case *ast.InfixExpression:
+		left := Eval(node.Left, env)
 		if isError(left) {
 			return left
 		}
-
-		right := Eval(node.Right)
-
+		right := Eval(node.Right, env)
 		if isError(right) {
 			return right
 		}
-
 		return evalInfixExpression(node.Operator, left, right)
+
 	case *ast.IfExpression:
-		return evalIfExpression(node)
+		return evalIfExpression(node, env)
+
+	case *ast.Identifier:
+		return evalIdentifier(node, env)
 	}
 
 	return nil
 }
 
-func evalProgram(stmts []ast.Statement) object.Object {
+func evalProgram(
+	stmts []ast.Statement,
+	env *object.Environment,
+) object.Object {
 	var result object.Object
 
 	for _, statement := range stmts {
-		result = Eval(statement)
+		result = Eval(statement, env)
 
 		switch result := result.(type) {
 		case *object.ReturnValue:
@@ -82,11 +94,14 @@ func evalProgram(stmts []ast.Statement) object.Object {
 	return result
 }
 
-func evalBlockStatement(block *ast.BlockStatement) object.Object {
+func evalBlockStatement(
+	block *ast.BlockStatement,
+	env *object.Environment,
+) object.Object {
 	var result object.Object
 
 	for _, statement := range block.Statements {
-		result = Eval(statement)
+		result = Eval(statement, env)
 
 		if result != nil {
 			rt := result.Type()
@@ -191,17 +206,20 @@ func evalIntegerInfixExpression(
 	}
 }
 
-func evalIfExpression(ie *ast.IfExpression) object.Object {
-	condition := Eval(ie.Condition)
+func evalIfExpression(
+	ie *ast.IfExpression,
+	env *object.Environment,
+) object.Object {
+	condition := Eval(ie.Condition, env)
 
 	if isError(condition) {
 		return condition
 	}
 
 	if isTruthy(condition) {
-		return Eval(ie.Consequence)
+		return Eval(ie.Consequence, env)
 	} else if ie.Alternative != nil {
-		return Eval(ie.Alternative)
+		return Eval(ie.Alternative, env)
 	} else {
 		return NULL
 	}
@@ -218,6 +236,19 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+func evalIdentifier(
+	node *ast.Identifier,
+	env *object.Environment,
+) object.Object {
+	val, ok := env.Get(node.Value)
+
+	if !ok {
+		return newError("identifier not found: " + node.Value)
+	}
+
+	return val
 }
 
 func newError(format string, a ...interface{}) *object.Error {
--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey-ball/evaluator"
 	"monkey-ball/lexer"
+	"monkey-ball/object"
 	"monkey-ball/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = "\n>> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
