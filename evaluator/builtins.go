39ea5b04770b622f34c1eda08314a7ede285a145
jmarchesini <john.marchesini@gmail.com>
2022-08-18 12:43:42 -0500

    4.5: hashes

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey-ball/object"
+import (
+	"fmt"
+	"monkey-ball/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -112,6 +115,16 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
--- token/token.go
+++ token/token.go
@@ -31,13 +31,13 @@ const (
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
-
-	LPAREN   = "("
-	RPAREN   = ")"
-	LBRACE   = "{"
-	RBRACE   = "}"
-	LBRACKET = "["
-	RBRACKET = "]"
+	COLON     = ":"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LBRACKET  = "["
+	RBRACKET  = "]"
 
 	// Keywords
 	FUNCTION = "FUNCTION"
