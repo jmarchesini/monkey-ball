12193ba5a6e865279e46c1444e0b492808ef04d2
jmarchesini <john.marchesini@gmail.com>
2022-11-30 20:35:29 -0600

    8: Refactor builtins

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,29 +1,12 @@
 package evaluator
 
 import (
-	"fmt"
 	"monkey-ball/object"
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": {
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of args. got=%d want=1",
-					len(args))
-			}
-
-			switch arg := args[0].(type) {
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("arg to `len` not supported, got %s",
-					args[0].Type())
-			}
-		},
-	},
+	"len":  object.GetBuiltinByName("len"),
+	"puts": object.GetBuiltinByName("puts"),
 
 	"first": {
 		Fn: func(args ...object.Object) object.Object {
@@ -115,16 +98,6 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
-		},
-	},
-
-	"puts": {
-		Fn: func(args ...object.Object) object.Object {
-			for _, arg := range args {
-				fmt.Println(arg.Inspect())
-			}
-
-			return NULL
 		},
 	},
 }
---
+++ object/buitins.go
@@ -0,0 +1,57 @@
+package object
+
+import "fmt"
+
+var Builtins = []struct {
+	Name    string
+	Builtin *Builtin
+}{
+	{
+		"len",
+		&Builtin{Fn: func(args ...Object) Object {
+			if len(args) != 1 {
+				return newError("wrong number of args. got=%d want=1",
+					len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *Array:
+				return &Integer{Value: int64(len(arg.Elements))}
+			case *String:
+				return &Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("arg to `len` not supported, got %s",
+					args[0].Type())
+			}
+		},
+		},
+	},
+	{
+		"puts",
+		&Builtin{Fn: func(args ...Object) Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return nil
+		},
+		},
+	},
+}
+
+// //////////////////////////////////////////////// //
+// ==================== Helpers =================== //
+// //////////////////////////////////////////////// //
+func newError(format string, a ...interface{}) *Error {
+	return &Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func GetBuiltinByName(name string) *Builtin {
+	for _, def := range Builtins {
+		if def.Name == name {
+			return def.Builtin
+		}
+	}
+
+	return nil
+}
