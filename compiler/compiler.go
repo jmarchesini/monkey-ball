b038a35e12bdea8f37f4fc2015835c18795bb971
jmarchesini <john.marchesini@gmail.com>
2022-12-05 21:26:29 -0600

    8: Compile builtins

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -38,9 +38,15 @@ func New() *Compiler {
 		previousInstruction: EmittedInstruction{},
 	}
 
+	symbolTable := NewSymbolTable()
+
+	for i, v := range object.Builtins {
+		symbolTable.DefineBuiltin(i, v.Name)
+	}
+
 	return &Compiler{
 		constants:   []object.Object{},
-		symbolTable: NewSymbolTable(),
+		symbolTable: symbolTable,
 		scopes:      []CompilationScope{mainScope},
 		scopeIndex:  0,
 	}
@@ -316,11 +322,8 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		if symbol.Scope == GlobalScope {
-			c.emit(code.OpGetGlobal, symbol.Index)
-		} else {
-			c.emit(code.OpGetLocal, symbol.Index)
-		}
+
+		c.loadSymbol(symbol)
 
 	// //////////////////////////////////////////////// //
 	// ================== Integrals =================== //
@@ -347,6 +350,17 @@ func (c *Compiler) Bytecode() *Bytecode {
 // //////////////////////////////////////////////// //
 // ==================== Helpers =================== //
 // //////////////////////////////////////////////// //
+
+func (c *Compiler) loadSymbol(s Symbol) {
+	switch s.Scope {
+	case GlobalScope:
+		c.emit(code.OpGetGlobal, s.Index)
+	case LocalScope:
+		c.emit(code.OpGetLocal, s.Index)
+	case BuiltinScope:
+		c.emit(code.OpGetBuiltin, s.Index)
+	}
+}
 
 func (c *Compiler) replaceLastPopWithReturn() {
 	lastPos := c.scopes[c.scopeIndex].lastInstruction.Position
